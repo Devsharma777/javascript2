c993f4b4d314d9ded09c53d4e6bd14ddb80c77f8
Devsharma777 <dev4cj2592@gmail.com>
2026-06-02 10:12:24 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 133445
+let accountEmail = "dev4567@gmail"
+var accountPassword = "122345"
+accountCity = "noida"
+let accountState
+
+// accountId = 2 not allowed
+
+accountEmail = "dev@5"
+accountPassword = "345"
+accountCity = "delhi"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity, accountState])
