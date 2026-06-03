637ce7112c587eaa1f0536ee5e546c2c053104a4
Devsharma777 <dev4cj2592@gmail.com>
2026-06-03 08:51:55 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// alert(3 + 3) // we are using nodejs not browser
+
+let name = "dev"
+let age = 21
+let isLoggedIn = true
+
+console.log(typeof "dev")
+console.log(typeof age)
+console.log(typeof null)
+
+
+
