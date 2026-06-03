12b38d47decc145b88e476b05f6ea04cf2209b3b
Devsharma777 <dev4cj2592@gmail.com>
2026-06-03 08:52:42 +0530

    Add files via upload

---
+++ 04_comparison.js
@@ -0,0 +1,19 @@
+//console.log(2 > 1)
+//console.log(2 >= 1)
+
+console.log("2" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null ==  0)
+
+console.log(undefined == 0)
+
+console.log("2" === 2)
+
+
+// memory stack and heap 
+
+let myYoutubenAME = "devsharma.com"
+
+
